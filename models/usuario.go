0b67642481356ced962de0b372c9ebdca55ebbe7
David <david.polania3@gmail.com>
2021-08-14 17:38:28 -0500

    Modelo de Usuarios errores corregidos

--- models/usuario.go
+++ models/usuario.go
@@ -8,15 +8,15 @@ import (
 
 /* usuario es el modelo de usuario de la base de datos MongoDB */
 type Usuario struct {
-	ID              primitive.ObjectID `bson:"_id, omitempty" 	json:"id"`
-	Nombre          string             `bson:"nombre" 			json:"nombre,omitempty"`
-	Apellidos       string             `bson:"apellidos" 		json:"apellidos,omitempty"`
-	FechaNacimiento time.Time          `bson:"fechaNacimiento"	json:"fechaNacimiento,omitempty"`
-	Email           string             `bson:"email" 			json:"email"`
-	Password        string             `bson:"password" 		json:"password,omitempty"`
-	Avatar          string             `bson:"avatar" 			json:"avatar,omitempty"`
-	Banner          string             `bson:"banner" 			json:"banner,omitempty"`
-	Biografia       string             `bson:"biography" 		json:"biography,omitempty"`
-	Ubicacion       string             `bson:"ubicacion" 		json:"ubicacion,omitempty"`
-	SitioWeb        string             `bson:"sitioWeb" 		json:"sitioWeb,omitempty"`
+	ID              primitive.ObjectID `bson:"_id, omitempty" json:"id"`
+	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
+	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
+	Email           string             `bson:"email" json:"email"`
+	Password        string             `bson:"password" json:"password,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner          string             `bson:"banner" json:"banner,omitempty"`
+	Biografia       string             `bson:"biography" json:"biography,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+	SitioWeb        string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
 }
