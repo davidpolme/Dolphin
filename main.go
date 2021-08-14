3202ed856df4777e46472ac9c6009dd977e38ceb
David <david.polania3@gmail.com>
2021-08-14 00:24:29 -0500

    main.go with the handlers and connection to de DB

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"Dolphin/db"
+	"Dolphin/handlers"
+)
+
 func main()  {
-	
+	if db.CheckConnection() == 0 {
+		log.Fatal("Sin conexion a la Base de datos")
+		return
+	}
+	handlers.Handlers()
 }
