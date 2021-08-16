1b7998d2e63a4de6745904c31dc8082fd33724ef
David <david.polania3@gmail.com>
2021-08-15 23:17:02 -0500

    Rutina de routers registro

---
+++ routers/register..go
@@ -0,0 +1,46 @@
+package routers
+
+import (
+	"Dolphin/db"
+	"Dolphin/models"
+	"encoding/json"
+	"net/http"
+)
+
+func Register(w http.ResponseWriter, r *http.Request  )  {
+	var t models.Usuario
+	
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Error en datos recibidos"+ err.Error(), 400)
+		return
+	}
+	if len(t.Email)==0 {
+		http.Error(w, "El email de usuario es requerido", 400)
+		return
+	}
+	if len(t.Password)<6 {
+		http.Error(w, "Debe especificar una contraseña de al menos 6 caracteres", 400)
+		return
+	}
+
+	_, encontrado, _ := db.CheckExistingUser(t.Email)
+	if encontrado == true {
+		http.Error(w, "Ya existe un usuario registrado con ese email", 400)	
+		return	
+	}
+
+	_, status , err := db.InsertRegister(t)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al inentar realizar el registo de usuario"+err.Error(),400)
+		return	
+	}
+
+	if status==false {
+		http.Error(w, "No se ha logrado insertar el registro al usuario", 400)
+		return
+	}
+
+
+	w.WriteHeader(http.StatusCreated)
+} 
